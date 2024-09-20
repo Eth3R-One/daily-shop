48bbe680392f73a414316ab06cfa20c027b2d1af
Eth3R-One <refaythii@gmail.com>
2024-09-20 14:45:25 +0600

    removed: comments

--- app/_components/products/HomepageProductSection.jsx
+++ app/_components/products/HomepageProductSection.jsx
@@ -1,18 +1,3 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-
 import { getProducts } from "@/app/actions/product-actions";
 import PorductsList from "./PorductsList";
 
