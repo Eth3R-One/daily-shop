13699abdcb810b99a7f5a669a0fa621aa53c3e80
Eth3R-One <refaythii@gmail.com>
2024-09-20 15:34:18 +0600

    add: footer component

---
+++ app/_components/FooterSection.jsx
@@ -0,0 +1,16 @@
+const FooterSection = () => {
+	return (
+		<footer aria-labelledby="footer-heading" className="bg-white">
+			<h2 id="footer-heading" className="sr-only">
+				Footer
+			</h2>
+			<div className="border-t border-gray-200 py-5  rounded-xl">
+				<p className="text-sm text-gray-500 text-center">
+					Copyright &copy; 2021 Your Company, Inc.
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default FooterSection;
