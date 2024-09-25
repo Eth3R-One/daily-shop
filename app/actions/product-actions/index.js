0fd88f36f610a54d23271eb4d9db949a7f00278d
Eth3R-One <refaythii@gmail.com>
2024-09-25 15:35:55 +0600

    add: total products action

--- app/actions/product-actions/index.js
+++ app/actions/product-actions/index.js
@@ -9,3 +9,13 @@ export const getProducts = async ({ SKIP = 0, LIMIT = 10 } = {}) => {
 		console.log(err);
 	}
 };
+
+export const getTotalProductsSize = async () => {
+	try {
+		const data = await fetch(`${process.env.API_BASE_URL}/products`);
+		const { total } = await data.json();
+		return total;
+	} catch (err) {
+		console.log(err);
+	}
+};
